7cd82d54fee2b9fae22aa115f6dc1348a4b832fe
Matthias Specht <sylence@btbsoft.org>
2023-08-04 22:45:09 +0200

    Add more tests to ensure analyzer does nothing when not configured

--- ArchitectureAnalyzer.Tests/ArchitectureAnalyzerTests.cs
+++ ArchitectureAnalyzer.Tests/ArchitectureAnalyzerTests.cs
@@ -1,10 +1,65 @@
-﻿namespace ArchitectureAnalyzer.Tests;
+﻿using ArchitectureAnalyzer.Tests.Helper;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+
+namespace ArchitectureAnalyzer.Tests;
 
 public sealed class ArchitectureAnalyzerTests
 {
 	[Fact]
+	public async Task DoesNothing_WhenNoCodeIsPresent()
+	{
+		var verifier = new CSharpAnalyzerTest<ArchitectureAnalyzer, FluentVerifier>
+		{
+			TestState =
+			{
+				Sources = { "" },
+				AdditionalFiles =
+				{
+					(
+						"architecture.rules.json", @"
+						{
+						""rules"": [
+							{
+								""mustImplement"": {
+								""interface"": { ""fullName"": ""System.IDisposable"" },
+								""forTypes"": { ""name"": ""*"" }
+							}}
+						]
+						}"
+					)
+				}
+			}
+		};
+
+		await verifier.RunAsync();
+	}
+
+	[Fact]
 	public async Task DoesNothing_WhenNoJsonFileIsFound()
 	{
 		await Verify.VerifyAnalyzerAsync("public class Foo {}");
+	}
+
+	[Fact]
+	public async Task DoesNothing_WhenNoRulesAreDefined()
+	{
+		var verifier = new CSharpAnalyzerTest<ArchitectureAnalyzer, FluentVerifier>
+		{
+			TestState =
+			{
+				Sources = { "public class Foo {}" },
+				AdditionalFiles =
+				{
+					(
+						"architecture.rules.json", @"
+						{
+						""rules"": []
+						}"
+					)
+				}
+			}
+		};
+
+		await verifier.RunAsync();
 	}
 }
